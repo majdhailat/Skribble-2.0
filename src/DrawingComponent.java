f1b1ae49777bf99604b8314acf0d967f4968c057
BusterBaxter <49295738+BusterBaxter@users.noreply.github.com>
2020-04-28 19:03:02 -0400

    Update DrawingComponent.java
    changed from line components to using circles

--- src/DrawingComponent.java
+++ src/DrawingComponent.java
@@ -10,13 +10,12 @@ class DrawingComponent implements Serializable{
     private static String toolType = PENCIL;
 
     //NON STATIC FIELDS
-    private int x1, y1, x2, y2, stroke;
+    private int cx, cy, stroke;
     private Color col;
-    public DrawingComponent(int x1, int y1, int x2, int y2){
-        this.x1 = x1;
-        this.y1 = y1;
-        this.x2 = x2;
-        this.y2 = y2;
+
+    public DrawingComponent(int cx, int cy){
+        this.cx = cx;
+        this.cy = cy;
         this.stroke = currentStrokeSize;
         if (toolType.equals(ERASER)){
             this.col = Color.white;
@@ -25,21 +24,13 @@ class DrawingComponent implements Serializable{
         }
     }
 
-    //NON STATIC METHODS
-    public int getX1() {
-        return x1;
+
+    public int getCx(){
+        return cx;
     }
 
-    public int getY1() {
-        return y1;
-    }
-
-    public int getX2() {
-        return x2;
-    }
-
-    public int getY2() {
-        return y2;
+    public int getCy(){
+        return cy;
     }
 
     public int getStroke() {
