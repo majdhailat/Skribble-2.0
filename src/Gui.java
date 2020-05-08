5219be2f0cc91feacd9836622ea44da67a7ec7c5
majdhailat <majdhailat64@gmail.com>
2020-05-07 21:49:06 -0400

    added exception listener

--- src/Gui.java
+++ src/Gui.java
@@ -4,10 +4,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.IOException;
 
 public class Gui extends JFrame {
     private Panel panel;
-    public Gui(Client client){
+    public Gui(Client client) throws IOException {
         super("Skribble");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(new Dimension(1280, 720));
