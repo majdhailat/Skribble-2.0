afe21bb51273a1b3d689475576c161eff92d7e6d
majdhailat <majdhailat64@gmail.com>
2020-05-08 16:27:20 -0400

    .

--- src/Gui.java
+++ src/Gui.java
@@ -33,8 +33,8 @@ public class Gui extends JFrame {
     class TickListener implements ActionListener {
         public void actionPerformed(ActionEvent evt) {
             if (panel != null && panel.ready) {
-                panel.repaint();
                 panel.updateFromClient();
+                panel.repaint();
             }
         }
     }
